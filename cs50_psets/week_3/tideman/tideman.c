134e9a364d5f61acd3cf9151ee4655e45c6df940
Abdisalam-Abdulahi <iidlex99@gmail.com>
2026-01-06 08:07:08 +0300

    sort_pair

--- cs50_psets/week_3/tideman/tideman.c
+++ cs50_psets/week_3/tideman/tideman.c
@@ -170,6 +170,28 @@ void add_pairs(void)
 void sort_pairs(void)
 {
     // TODO
+    int swap = 0;
+    for (int i = 0; i < pair_count; i++)
+    {
+        for (int j = 0; j < pair_count - 1; j++)
+        {
+            if ((preferences[pairs[j].winner][pairs[j].loser] - preferences[pairs[j].loser][pairs[j].winner]) < (preferences[pairs[j + 1].winner][pairs[j + 1].loser] - preferences[pairs[j + 1].loser][pairs[j + 1].winner]))
+            {
+                pair temp = pairs[j];
+                pairs[j] = pairs[j + 1];
+                pairs[j + 1] = temp;
+                swap = 1;
+            }
+        }
+        if (swap == 0)
+        {
+            return;
+        }
+        else
+        {
+            swap = 0;
+        }
+    }
     return;
 }
 
