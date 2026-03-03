10b43df5527e4177cdff22cf6e3edaa257c190aa
Abdisalam-Abdulahi <iidlex99@gmail.com>
2026-03-03 09:58:51 +0300

    -

--- cs50_examples/week_9/hello/app.py
+++ cs50_examples/week_9/hello/app.py
@@ -2,10 +2,11 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template("index.html")
+    if request.method == "POST":
+        return render_template("greet.html", name=request.form.get("name"))
+    else:
+        return render_template("index.html")
 
-@app.route("/greet")
-def greet():
-    return render_template("greet.html", name=request.args.get("name", "world"))
+    
